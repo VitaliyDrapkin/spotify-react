ca7e3092a6108d17260ae01a56c524c8c6fceb20
Vitaliy <drapkinvitaliy@gmail.com>
2021-03-29 20:08:58 +0300

    edited styles for logo, add playlist component width css

--- src/modules/main/pages/Home/HomePage.tsx
+++ src/modules/main/pages/Home/HomePage.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import Playlist from "../../components/Playlist";
 import s from "./HomePage.module.css";
 
+const playlist2 = {
+  description: "Start your day by chilling",
+  image_url: "https://i.scdn.co/image/ab67706f000000035337e18dc6803780d806efba",
+  name: "Saturday Morning Can’t Be Better",
+  playlist_id: "3533",
+};
+const playlist = {
+  description: "Listen to the best songs from the 60's",
+  image_url: "https://i.scdn.co/image/ab67706f000000035337e18dc6803780d806efba",
+  name: "Best of 60's",
+  playlist_id: "3533",
+};
 export default function HomePage() {
-  return <div className={s.HomePage}>Home</div>;
+  return (
+    <div className={s.HomePage}>
+      <Playlist
+        image={playlist.image_url}
+        title={playlist.name}
+        description={playlist.description}
+      ></Playlist>
+    </div>
+  );
 }
